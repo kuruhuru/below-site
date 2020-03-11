07985aa8b6cd80dbd8503c86821f888cef2ba5a1
Peter Matyukov <kuruhuru@gmail.com>
2020-03-11 14:26:35 +0700

    Added deploy scripts

---
+++ deploy.bat
@@ -0,0 +1,25 @@
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+rem Clear Public folder
+cd public
+git checkout master
+git pull
+git rm -r .
+cd ..
+
+rem Build the project. if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -b https://ai-cluster.ru/below
+
+rem Go To Public folder
+cd public
+rem Add changes to git.
+git add .
+
+rem Commit changes.
+git commit -m "Update site"
+
+rem Push source and build repos.
+git push origin master
+
+rem Come Back up to the Project Root
+cd ..
---
+++ deploy.sh
@@ -0,0 +1,25 @@
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Clear Public folder
+cd public
+git checkout master
+git pull
+git rm -r .
+cd ..
+
+# Build the project. if using a theme, replace with `hugo -t <YOURTHEME>`
+./hugo -b https://ai-cluster.ru/below
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m "Update site"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
